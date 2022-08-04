7d663fbe1367bda493209a43945ca46c942daff4
Andg3ky <andrewleah55@outlook.com>
2022-08-04 23:27:45 +0000

    remove open claims

--- src/OpenClaims/OpenClaimsRow.js
+++
@@ -1,8 +0,0 @@
-const OpenClaimsRow = (props) => {
-
-    return <tr><th>{props.id}</th><th>{props.policy}</th><th>{props.surname}</th><th>{props.status}</th>
-    <th><button className="openBtn">{props.button}</button></th></tr>
-
-}
-
-export default OpenClaimsRow;
