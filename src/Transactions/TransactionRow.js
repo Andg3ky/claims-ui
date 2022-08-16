0daf1f86dcdeeaf992ae5cdd0a352c65d59fa73f
Andg3ky <andrewleah55@outlook.com>
2022-08-16 22:13:14 +0000

    pol number display

--- src/Transactions/TransactionRow.js
+++ src/Transactions/TransactionRow.js
@@ -1,6 +1,6 @@
 const TransactionRow = (props) => {
 
-    return <tr><th>{props.id}</th><th>{props.policynumber}</th><th>{props.customer}</th><th>{props.status}</th>
+    return <tr><th>{props.id}</th><th>{props.policyNumber}</th><th>{props.customer}</th><th>{props.status}</th>
     <th><button className="showDetailBtn">Show Detail</button></th></tr>
 
 }
