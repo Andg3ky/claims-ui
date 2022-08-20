9ce69972e91748b6750b204a0bad6f7a90f92701
Andg3ky <andrewleah55@outlook.com>
2022-08-20 18:04:31 +0000

    new claim page updates

--- src/NewClaim/NewClaim.js
+++ src/NewClaim/NewClaim.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react'
+import { useReducer, useState } from 'react'
 import './NewClaims.css'
 
 const NewClaim = () => {
@@ -32,6 +32,13 @@ const NewClaim = () => {
       console.log(newClaim);
     }
 
+    //Radio Button trigger of additional fields
+    const [selectedRadio, setSelectedRadio] = useState("");
+    
+    const changeHandler = e => {
+      setSelectedRadio(e.target.value);
+    };
+
     //Register New Claim entry form
     return( <div> 
         <div className="newContainer">
@@ -40,20 +47,23 @@ const NewClaim = () => {
 
        {/* Policy Number */}
         <label htmlFor="registerPolicyNumber">Policy Number *</label>
-        <input type="text"  placeholder="(i.e 9 digit policy number)" name="registerpolicyNumber" id="registerpolicyNumber" 
+        <input type="text"  placeholder="(i.e 9 digit policy number)" name="policyNumber" id="policyNumber" 
          onChange={handleChange} value={policyNumber} />
 
-        {/* Type of Insurance */}
-        <label>Type of Insurance *</label>
+         {/* Type of Insurance */}
+        <label>Type of Insurance </label>
         <div className="radio-container">
           
-          <input type="radio" id="property" value="property" name="insuranceType" onChange={handleChange} />
+          <input type="radio" id="property" value="property" name="insuranceType" 
+          checked={selectedRadio === "property"} onChange={changeHandler} />
           <label className="radio-label" htmlFor="property">property</label>
 
-          <input type="radio" id="motor" value="motor" name="insuranceType" onChange={handleChange} />
+          <input type="radio" id="motor" value="motor" name="insuranceType" 
+          checked={selectedRadio === "motor"} onChange={changeHandler} />
           <label className="radio-label" htmlFor="motor">motor</label>
 
-          <input type="radio" id="pet" value="pet" name="insuranceType" onChange={handleChange} />
+          <input type="radio" id="pet" value="pet" name="insuranceType" 
+          checked={selectedRadio === "pet"} onChange={changeHandler} />
           <label className="radio-label" htmlFor="pet">pet</label>
         </div>
 
@@ -61,29 +71,35 @@ const NewClaim = () => {
         <label htmlFor="title">Title *</label>
         <select id="title" name="title" onChange={handleChange} value={title}>
           <option value="" defaultValue>-- select --</option>
-          <option value="mr">Mr</option>
-          <option value="ms">Mrs</option>
-          <option value="mx">Mx</option>
+          <option value="Mr">Mr</option>
+          <option value="Ms">Mrs</option>
+          <option value="Mx">Mx</option>
         </select>
 
         {/* First Name */}
         <label htmlFor="firstName">First Name *</label>
         <input type="text" placeholder="first name" name="firstName" id="firstName" onChange={handleChange} value={firstName} />
+
         {/* Surname */}
         <label htmlFor="registerSurname">Surname *</label>
-        <input type="text" placeholder="surname" name="registerSurname" id="registersurname" onChange={handleChange} value={surName} />
+        <input type="text" placeholder="surname" name="surName" id="surName" onChange={handleChange} value={surName} />
+
         {/* Claim Start Date */}
         <label htmlFor="Claim Start">Claim Start Date</label>
         <input type="date" id="claimStart" name="claimStart" onChange={handleChange} value={claimStart}/>
+
         {/* Claim Amount */}
         <label htmlFor="Claim Amount">Estimated Claim Amount (approx)</label>
         <input type="text" placeholder="claim amount - $0.00" name="claimAmount" id="claimAmount" onChange={handleChange} value={claimAmount}/>
+
         {/* Reason for Claim */}
         <label htmlFor="Claim Reason">Claim Reason (Enter details)</label>
         <input type="text" placeholder="reason for claim" name="claimReason" id="claimReason" onChange={handleChange} value={claimReason} />
+
         {/* Description of Incident leading up to Claim */}
         <label htmlFor="Incident Description">Incident Description (Enter details)</label>
-        <textarea name="incidentDescription" placeholder="Enter incident description" id="incidentDescription" onChange={handleChange} value={incidentDescription} rows="4" cols="62"></textarea>
+        <textarea name="incidentDescription" placeholder="Enter incident description" id="incidentDescription" onChange={handleChange} 
+        value={incidentDescription} rows="4" cols="62"></textarea>
 
         <div>
             <h1 className="insuranceSpecificTitle">Insurance Type Specific Fields</h1>
@@ -92,30 +108,37 @@ const NewClaim = () => {
         <br></br>
 
         {/* Property Address - if property radio button is selected */}
-        <div id="myPropertyDIV">
-          <label htmlFor="propertyAddress">Property Address</label>
-          <textarea name="propertyAddress" placeholder="Enter property address" id="propertyAddress" onChange={handleChange} value={propertyAddress}  rows="4" cols="60"></textarea>
+        <div aria-hidden={selectedRadio !== "property" ? true : false}>
+          <div id="myPropertyDIV">
+            <label htmlFor="propertyAddress">Property Address</label>
+            <textarea name="propertyAddress" placeholder="Enter property address" id="propertyAddress" onChange={handleChange} 
+           value={propertyAddress}  rows="4" cols="60"></textarea>
+          </div>
         </div>
 
         {/* Vehicle Make - if motor radio button is selected */}
-        <div id="myMotorDIV">
-          <label htmlFor="vehicleMake">Vehicle Make</label>
-          <input type="text" name="vehicleMake" placeholder="vehicle make" id="vehicleMake" onChange={handleChange} value={vehicleMake} />
+        <div aria-hidden={selectedRadio !== "motor" ? true : false}>
+          <div id="myMotorDIV">
+           <label htmlFor="vehicleMake">Vehicle Make</label>
+            <input type="text" name="vehicleMake" placeholder="vehicle make" id="vehicleMake" onChange={handleChange} value={vehicleMake} />
         {/* Vehicle Model - if motor radio button is selected */}
-          <label htmlFor="vehicleModel">Vehicle Model</label>
-           <input type="text" name="vehicleModel" placeholder="vehicle model" id="vehicleModel" onChange={handleChange} value={vehicleModel} />
+            <label htmlFor="vehicleModel">Vehicle Model</label>
+            <input type="text" name="vehicleModel" placeholder="vehicle model" id="vehicleModel" onChange={handleChange} value={vehicleModel} />
         {/* Vehicle Year - if motor radio button is selected */}
-          <label htmlFor="vehicleYear">Vehicle Year</label>
-          <input type="number" name="vehicleYear" placeholder="vehicle year" id="vehicleYear" onChange={handleChange} value={vehicleYear} />
+            <label htmlFor="vehicleYear">Vehicle Year</label>
+            <input type="number" name="vehicleYear" placeholder="vehicle year" id="vehicleYear" onChange={handleChange} value={vehicleYear} />
+          </div>
         </div>
 
         {/* Pet Type - if pet radio button is selected */}
-        <div id="myPetDIV">
-         <label htmlFor="petType">Pet Type</label>
-         <input type="text" name="petType" placeholder="pet type(i.e dog, cat)" id="petType" onChange={handleChange} value={petType} />
+        <div aria-hidden={selectedRadio !== "pet" ? true : false}>
+          <div id="myPetDIV">
+            <label htmlFor="petType">Pet Type</label>
+            <input type="text" name="petType" placeholder="pet type(i.e dog, cat)" id="petType" onChange={handleChange} value={petType} />
         {/* Pet Breed - if pet radio button is selected */}
-         <label htmlFor="petBreed">Pet Breed</label>
-         <input type="text" name="petBreed" placeholder="pet breed(i.e. terrior, bulldog, maine coon)" id="petBreed" onChange={handleChange} value={petBreed} />
+            <label htmlFor="petBreed">Pet Breed</label>
+            <input type="text" name="petBreed" placeholder="pet breed(i.e. terrior, bulldog, maine coon)" id="petBreed" onChange={handleChange} value={petBreed} />
+          </div>
         </div>
 
         {/* Register Button */}
